8bf06dac42d5421326b86eeee50d991903cdfa61
Tatiana Ivanyshkina <Ita86@bk.ru>
2024-02-19 22:58:02 +0400

    Finished Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+
+string GetStringFromCharArray(char[,] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result = result + array[i, j];
+        }
+    }
+    return result;
+}
+
+char[,] charMatrix = new char[,] { { 'a', 'b', 'c' }, { 'd', 'e', 'f' } };
+string res = GetStringFromCharArray(charMatrix);
+Console.WriteLine(res);
