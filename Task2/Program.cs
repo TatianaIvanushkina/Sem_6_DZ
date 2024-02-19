902be0f7a05d58ab28a43d85e12a21cdc4806442
Tatiana Ivanyshkina <Ita86@bk.ru>
2024-02-19 23:06:40 +0400

    Finished Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+Console.Write("Vvedite stroku: ");
+string inputString = Console.ReadLine();
+inputString = inputString.ToLower();
+Console.Write(inputString);
